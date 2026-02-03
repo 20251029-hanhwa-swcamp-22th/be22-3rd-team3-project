e1a5eeb424fbb1f45abc1747555db52b86d485a7
chanjin346 <ckswls2231@gmail.com>
2026-02-04 00:22:06 +0900

    티어표 수정 및 점수계산 로직 수정

--- src/stores/quiz.js
+++ src/stores/quiz.js
@@ -206,7 +206,7 @@ export const useQuizStore = defineStore('quiz', () => {
         let questionScore = 0
         if (isCorrect) {
             // 기본 10점 + 빨리 맞출수록 추가 점수 (남은 시간 * 2)
-            questionScore = 10 + (questionTimeRemaining.value * 2)
+            questionScore = 10
             score.value += questionScore // 총점 누적
         }
 
@@ -272,15 +272,15 @@ export const useQuizStore = defineStore('quiz', () => {
 
         // 최종 점수에 따른 티어 산정
         const finalScore = score.value
-        if (finalScore >= 180) {
+        if (finalScore >= 100) {
             tier.value = '멘사'
-        } else if (finalScore >= 140) {
+        } else if (finalScore >= 90) {
             tier.value = '수재'
-        } else if (finalScore >= 100) {
-            tier.value = '우등생'
         } else if (finalScore >= 60) {
+            tier.value = '우등생'
+        } else if (finalScore >= 40) {
             tier.value = '모범생'
-        } else if (finalScore >= 30) {
+        } else if (finalScore >= 20) {
             tier.value = '평범'
         } else {
             tier.value = '노력필요'
