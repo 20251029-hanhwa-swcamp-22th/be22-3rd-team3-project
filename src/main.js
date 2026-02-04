125615b11a8e8a524981d84ed0763f423087c69a
ikth-kim <ikth.kim@gmail.com>
2026-02-05 01:10:37 +0900

    fix : 자동 로그인 문제 해결

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import App from './App.vue'
 import './assets/styles/main.css'
 import 'galmuri/dist/galmuri.css'
 
+import { setRouter } from "@/api/axios.js";
+
 const app = createApp(App)
 const pinia = createPinia()
 
